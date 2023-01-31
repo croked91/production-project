6fd46b154aaebec6bb5d65766c3cbabaab130455
Croked1991 <atik.egor@gmail.com>
2023-01-31 20:13:15 +0300

    added config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,27 @@
+import { Configuration } from "webpack";
+import { buildDevServer } from "./buildDevServer";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import { BuildOptions } from "./types/config";
+
+export const buildWebpackConfig = (options: BuildOptions): Configuration => {
+  const { paths, mode, isDev } = options;
+
+  return {
+    entry: paths.entry,
+    mode,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(options),
+    },
+    resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
+};
